813157edb55b0ad49f56e560ab36dbfa6ab2601b
e1b21013 <e1b21013@st.oit.ac.jp>
2023-10-10 14:21:59 +0900

    初期化

---
+++ src/main/java/oit/is/team08/lec03/Lec03Application.java
@@ -0,0 +1,13 @@
+package oit.is.team08.lec03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lec03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lec03Application.class, args);
+	}
+
+}
